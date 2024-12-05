b070827f871460eabe0ce88360a3ca0734b0ede2
Himansu Sekhar Lenka <himansu.lenka173@gmail.com>
2024-12-06 01:05:17 +0530

    ownerProfile page half done upto the profile image setup and data update form

--- frontend/ui/allProperties.php
+++ frontend/ui/allProperties.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../../bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css"/>
+    
     <style>
         .highlightEle {
             color: #e84545;
@@ -49,21 +51,13 @@
             <div class="row p-0 m-0">
                 <div class="col-6 d-md-none d-block">
                     <span class="px-4 btn border rounded fw-semibold shadow-sm" onclick="toggleFilters()"
-                        style="cursor:pointer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
-                            <path
-                                d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z" />
-                        </svg> Filter</span>
+                        style="cursor:pointer"><i class="bi bi-funnel"></i> Filter</span>
                 </div>
                 <div class="col-6 d-md-none text-end">
                     <div class="btn-group">
                         <button type="button" class="btn dropdown-toggle px-2 border rounded fw-semibold shadow-sm"
                             data-bs-toggle="dropdown" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-filter" viewBox="0 0 16 16">
-                                <path
-                                    d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                            </svg> Sort By
+                            <i class="bi bi-filter"></i> Sort By
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li><button class="dropdown-item" type="button">Popularity</button></li>
@@ -95,47 +89,6 @@
                     </div>
                 </div>
                 <div class="d-flex flex-wrap justify-content-around" id="propertyCardContainer">
-                    <div class="card property-card border-0 overflow-hidden mb-4" style="cursor:pointer;">
-                            <div id="propertyImg" class="carousel slide w-100">
-                                <div class="carousel-inner h-100 w-100">
-                                    <div class="carousel-item active">
-                                        <img src="https://imgs.search.brave.com/r3lzftQBeeNNKGchoyt7bQCTUmdgWJrxa772Vv6gOOk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzM3LzU0LzIz/LzM2MF9GXzQzNzU0/MjM2NF9yVUtSVURs/T1EyWm1WcnNpak5G/VWZyaHhaTzdqeU9G/Zy5qcGc" class="card-img" alt="ax" height="200px">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="https://imgs.search.brave.com/BURrOYrppSL3YBxY0ZIfp-r45A0AOzmKwaPYbPCQFD4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNy8w/MS8wNy8xNy80OC9p/bnRlcmlvci0xOTYx/MDcwXzY0MC5qcGc" class="card-img" alt="ax" height="200px">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="https://imgs.search.brave.com/MRIsvkNXzCHqfkoEJvWn5aLQ7Ozr-UPjlpw5aev76A0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzU3LzY4Lzgx/LzM2MF9GXzU3Njg4/MTc1X1BlSXNEOHpz/a013b2JiTTBOSVZN/SUlJMG5SZWdXc2Fs/LmpwZw" class="card-img" alt="ax" height="200px">
-                                    </div>
-                                </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#propertyImg"
-                                    data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon bg-secondary rounded-circle" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#propertyImg"
-                                    data-bs-slide="next">
-                                    <span class="carousel-control-next-icon bg-secondary rounded-circle" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
-                                </button>
-                            </div>
-                            <div class="card-body p-2 property-body bg-white">
-                                <h5 class="card-title text-primary-emphasis">Rajinder Nagar</h5>
-                                <p class="card-text mb-1 fs-6">
-                                    <img src="../images/bed.png" alt="img" width="20rem">
-                                    <span class="ms-1">3 BHK Flat for Sale in Sundarpada, Bhubaneswar</span>
-                                </p>
-                                <p class="card-text mb-1 fs-4 text-primary-emphasis m-0 fw-semibold">&#8377; 5.87L</p>
-                                <div class="row g-0">
-                                    <p class="col-6 mb-0 fw-medium">8087 Sq.Ft</p>
-                                    <p class="col-6 mb-0 fw-medium text-end">Semi-Furnished</p>
-                                </div>
-                                <div class="row g-0">
-                                    <p class="col-6 mb-0" style="font-size:0.9rem">Build-up Area</p>
-                                    <p class="col-6 mb-0 text-end" style="font-size:0.9rem">Furnishing Status</p>
-                                </div>
-                            </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -192,6 +145,9 @@
                 </div>
             `);            
         }
+        $(".property-card").click(()=>{
+            window.location="propertyDetails.php"
+        })
         function highlightSortBy(ele) {
             $(".sortByElement").removeClass("highlightEle");
             $(ele).addClass("highlightEle");
--- frontend/ui/propertyDetails.php
+++ frontend/ui/propertyDetails.php
@@ -6,28 +6,40 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Document</title>
    <link rel="stylesheet" href="../../bootstrap/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css"/>
+   <style>
+      .property-info{
+         width:33%;
+         margin-top:7px;
+      }
+      @media only screen and (max-width:567px) {
+         .property-info{
+            width:50%;
+         }
+      }
+   </style>
 </head>
 
 <body>
-   <div class="container-fluid">
-      <div class="row">
+   <div class="container">
+      <div class="row mt-2">
          <div class="col-md-7">
-            <div id="propertyImg" class="carousel slide w-100">
+            <div id="propertyImg" class="carousel slide w-100" data-bs-ride="carousel" pause interval="2">
                <div class="carousel-inner h-100 w-100">
                   <div class="carousel-item active">
                      <img
                         src="https://imgs.search.brave.com/r3lzftQBeeNNKGchoyt7bQCTUmdgWJrxa772Vv6gOOk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzM3LzU0LzIz/LzM2MF9GXzQzNzU0/MjM2NF9yVUtSVURs/T1EyWm1WcnNpak5G/VWZyaHhaTzdqeU9G/Zy5qcGc"
-                        class="card-img" alt="ax" height="400px">
+                        class="card-img rounded" alt="ax" height="400px">
                   </div>
                   <div class="carousel-item">
                      <img
                         src="https://imgs.search.brave.com/BURrOYrppSL3YBxY0ZIfp-r45A0AOzmKwaPYbPCQFD4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNy8w/MS8wNy8xNy80OC9p/bnRlcmlvci0xOTYx/MDcwXzY0MC5qcGc"
-                        class="card-img" alt="ax" height="400px">
+                        class="card-img rounded" alt="ax" height="400px">
                   </div>
                   <div class="carousel-item">
                      <img
                         src="https://imgs.search.brave.com/MRIsvkNXzCHqfkoEJvWn5aLQ7Ozr-UPjlpw5aev76A0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzU3LzY4Lzgx/LzM2MF9GXzU3Njg4/MTc1X1BlSXNEOHpz/a013b2JiTTBOSVZN/SUlJMG5SZWdXc2Fs/LmpwZw"
-                        class="card-img" alt="ax" height="400px">
+                        class="card-img rounded" alt="ax" height="400px">
                   </div>
                </div>
                <button class="carousel-control-prev" type="button" data-bs-target="#propertyImg" data-bs-slide="prev">
@@ -42,33 +54,198 @@
          </div>
          <div class="col-md-5">
             <div class="card-body px-2 px-md-0 pe-md-1 p-0">
-               <p class="card-text fw-semibold fs-sm-5 fs-6 m-0 p-0 text-primary-emphasis">Property
+               <p class="card-title fw-semibold fs-3 m-0 p-0 text-primary-emphasis">Property
                   Name</p>
-               <p class="card-text mb-1 fs-6"><img src="../images/bed.png" alt="img" width="20rem"> 2
-                  BHK Flat for Sale in Sundarpada, Bhubaneswar</p>
-               <p class="card-text mb-1 fs-5 text-primary-emphasis m-0 fw-semibold">&#8377; 5.87L</p>
-               <div class="row">
-                  <p class="col-6 mb-0 fw-medium">8087 Sq.Ft</p>
-                  <p class="col-6 mb-0 fw-medium">Semi-Furnished</p>
+               <p class="card-text mb-1 fs-5">2 BHK Flat for Sale in Sundarpada, Bhubaneswar</p>
+               <p class="card-text mb-1 fs-3 text-primary-emphasis m-0 fw-semibold">&#8377; 5.87 L</p>
+               <p class="card-text mb-1 fs-6">
+                  <img src="../images/bedroom.png" alt="img" width="23rem">
+                  <span class="ms-1"> 3 BedRooms</span>
+               </p>
+               <p class="card-text mb-1 fs-6">
+                  <img src="../images/sofa.png" alt="img" width="23rem">
+                  <span class="ms-1"> Semi-Furnished</span>
+               </p>
+               <p class="card-text mb-1 fs-6">
+                  <img src="../images/area.png" alt="img" width="23rem">
+                  <span class="ms-1"> 8768 Sq.ft (Build up Area)</span>
+               </p>
+               <p class="card-text mb-1 fs-6">
+                  <i class="bi bi-geo-alt fs-4"></i>
+                  <span class="ms-1"> Bhubaneswa, Odisha</span>
+               </p>
+               <div class="mt-3 d-flex align-items-center justify-content-between">
+                  <div class="d-flex align-items-center">
+                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtuphMb4mq-EcVWhMVT8FCkv5dqZGgvn_QiA&s" alt="user" height="35px" width="35px" class="rounded-circle">
+                     <p class="card-text ms-2 fw-semibold fs-6">Chandrasekhar Singh</p>
+                  </div>
+                  <div class="d-flex align-items-center">
+                  <i class="bi bi-telephone-fill fs-6"></i>
+                  <p class="card-text ms-2 fw-semibold fs-6">98751678231</p>
+                  </div>
                </div>
-               <div class="row">
-                  <p class="col-6 mb-0" style="font-size:0.9rem">Build-up Area</p>
-                  <p class="col-6 mb-0" style="font-size:0.9rem">Furnishing Status</p>
+            </div>
+         </div>
+      </div>
+      <div class="row mt-3 pb-3">
+         <div class="col-md-7">
+            <div class="card border-0 shadow">
+               <div class="fs-4 fw-semibold mx-2 p-2 border-bottom border-3">Key Highlights</div>
+               <div class="d-flex flex-wrap justify-content-evenly align-item-center mt-3">
+                  <div class="card rounded-pill p-2 m-1">Near City Center</div>
+                  <div class="card rounded-pill p-2 m-1">Affordable</div>
+                  <div class="card rounded-pill p-2 m-1">Spacious</div>
+                  <div class="card rounded-pill p-2 m-1">Well ventilated</div>
+                  <div class="card rounded-pill p-2 m-1">Well Maintained</div>
                </div>
-               <div class="mt-1">
-                  <p class="card-text lh-sm" style="font-size:0.9rem">
-                        <?php
-                           $description="Krishna Enclave - 2 BHK Flats for Sale in Sundarpada, Bhubaneswar. Searching for the perfect home for your family in a prime location.";
-                           echo strlen($description)>180?substr($description,0,180)."...":$description
-                        ?>
-                  </p>
+               <div class="px-3">
+                  <p class="card-text">Experience the true essence of comfortable living at this 2 BHK semi-furnished apartment in Manjadi, Tiruvalla.</p>
+                  <ul>
+                     <li>Rest easy knowing that 24x7 security services are in place to ensure your safety and peace of mind.</li>
+                     <li>Situated in the renowned SAI property, this impeccable property is available for sale at a price of 42 Lac.</li>
+                     <li>The living spaces here are tastefully designed with a focus on functionality and optimum space utilization.</li>
+                     <li>The prime location of this property makes daily commuting a breeze, with easy access to all the essential amenities and services of the city.</li>
+                     <li>Situated in the renowned SAI property, this impeccable property is available for sale at a price of 42 Lac.</li>
+                  </ul>
                </div>
+            </div>
+            <div class="card border-0 shadow my-3 pb-3">
+               <div class="fs-4 fw-semibold mx-2 p-2 border-bottom border-3">Property Information</div>
+               <div class="mt-3 d-flex flex-wrap px-2">
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Listing Type</p>
+                        <p class="fs-6 fw-semibold m-0">Rent</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Building Type</p>
+                        <p class="fs-6 fw-semibold m-0">Residential</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Property Type</p>
+                        <p class="fs-6 fw-semibold m-0">Villa</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">City</p>
+                        <p class="fs-6 fw-semibold m-0">Pathanamthitta</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Locality</p>
+                        <p class="fs-6 fw-semibold m-0">Adoor</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Price</p>
+                        <p class="fs-6 fw-semibold m-0">52 L</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Area</p>
+                        <p class="fs-6 fw-semibold m-0">8782 Sq.Ft</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Number of Rooms</p>
+                        <p class="fs-6 fw-semibold m-0">5</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Number of Bathroo</p>
+                        <p class="fs-6 fw-semibold m-0">2</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">View</p>
+                        <p class="fs-6 fw-semibold m-0">8782 Sq.Ft</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Balcony</p>
+                        <p class="fs-6 fw-semibold m-0">Individual</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Power Back-up</p>
+                        <p class="fs-6 fw-semibold m-0">No Back-up</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Total Floor Count</p>
+                        <p class="fs-6 fw-semibold m-0">2</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Balcony</p>
+                        <p class="fs-6 fw-semibold m-0">Individual</p>
+                     </div>
+                  </div>
+                  <div class="property-info p-0">
+                     <div class="border-bottom mx-3">
+                        <p class="fs-6 text-gray m-0">Facing</p>
+                        <p class="fs-6 fw-semibold m-0">East</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-5">
+            <div class="shadow rounded p-1" id="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3740.7806764633715!2d85.8037610752383!3d20.350677281134352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1908e064769e73%3A0x9288172f3a98c7a4!2sSilicon%20University!5e0!3m2!1sen!2sin!4v1733336564038!5m2!1sen!2sin" width="100%" height="300px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="card shadow mt-3 pb-3" id="simillarPropertyContainer">
+               <div class="fs-4 fw-semibold mx-2 p-2 border-bottom border-3">Simillar Properties</div>
             </div>
          </div>
       </div>
    </div>
    </div>
    <script src="../../bootstrap/bootstrap.bundle.min.js"></script>
+   <script src="../../jQuery/jquery-3.7.1.min.js"></script>
+   <script>
+      for(let i=0;i<6;i++){
+            $("#simillarPropertyContainer").append(`
+               <div class="row mt-3 g-0 px-2" onMouseOver="this.style.backgroundColor='#f5f5f5'" onMouseOut="this.style.backgroundColor=''" style="cursor:pointer;">
+                  <div class="col-md-2 col-3">
+                        <img src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?cs=srgb&dl=pexels-binyaminmellish-186077.jpg&fm=jpg" alt="img" width="70px" class="rounded">
+                  </div>
+                  <div class="col-md-4 col-sm-5 col-6 d-flex align-items-center flex-wrap overflow-hidden">
+                        <div class="d-flex align-items-center w-100 overflow-hidden" style="font-size:0.8rem">
+                           4 BHK Appartment 
+                        </div>
+                        <div class="d-flex align-items-center w-100 overflow-hidden" style="font-size:0.8rem">
+                           Bhubaneswar
+                        </div>
+                  </div>
+                  <div class="col-sm-2 d-sm-flex align-items-center overflow-hidden d-none" style="font-size:0.8rem">
+                        8976 Sq.Ft
+                  </div>
+                  <div class="col-md-2 d-md-flex align-items-center overflow-hidden d-none" style="font-size:0.8rem">
+                        Furnished
+                  </div>
+                  <div class="col-sm-2 col-3 d-flex align-items-center overflow-hidden" style="font-size:0.8rem">
+                  &#8377; 1.07L
+                  </div>
+               </div>
+            `)
+      }
+   </script>
 </body>
 
 </html>
